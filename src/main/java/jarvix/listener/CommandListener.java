38a6b357acf5c0c6bde8a570f42022172dd676cc
big_Xplosion <bigxplosion44@gmail.com>
2015-03-02 22:48:49 +0100

    Added basic listeners (no implementation)

--- src/main/java/jarvix/listener/CommandListener.java
+++ src/main/java/jarvix/listener/CommandListener.java
@@ -1,5 +1,13 @@
 package jarvix.listener;
 
-public class CommandListener {
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.MessageEvent;
 
+public class CommandListener extends ListenerAdapter<PircBotX> {
+
+	@Override
+	public void onMessage(MessageEvent<PircBotX> event) throws Exception {
+
+	}
 }
--- src/main/java/jarvix/listener/PrivateCommandListener.java
+++ src/main/java/jarvix/listener/PrivateCommandListener.java
@@ -1,5 +1,13 @@
 package jarvix.listener;
 
-public class PrivateCommandListener {
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
 
+public class PrivateCommandListener extends ListenerAdapter<PircBotX> {
+
+	@Override
+	public void onPrivateMessage(PrivateMessageEvent<PircBotX> event) throws Exception {
+		super.onPrivateMessage(event);
+	}
 }
